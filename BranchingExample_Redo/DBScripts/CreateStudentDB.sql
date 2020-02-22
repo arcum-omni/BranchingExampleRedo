5f7cf325fabdb8d223923d71cd075d25da11a30d
arcum-omni <travis.l.eiler@gmail.com>
2020-02-22 09:03:47 -0800

    Created Student DB

---
+++ BranchingExample_Redo/DBScripts/CreateStudentDB.sql
@@ -0,0 +1,13 @@
+﻿create database Student
+go
+
+use Student
+go
+
+create table Student
+(
+	FirstName		varchar(20)		not null
+	,LastName		varchar(30)		not null
+	,DateOfBirth	date			null
+	,Email			varchar(320)	null
+)
